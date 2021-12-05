1e10c759529d4d7dadda2a37777cc23fa31bca87
nando <muhammad.rezki.ananda@gmail.com>
2021-12-05 10:53:12 +0700

    Blade Templating Engine

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>WPU Blog | Posts</title>
-</head>
-<body>
-<h1>Halaman Posts</h1>
-</body>
-</html>
+@extends('layouts.main')
+
+@section('container')
+
+    @foreach($posts as $post)
+        <article class="mb-5">
+            <h2>
+                <a href="/posts/{{ $post['slug'] }}">
+                    {{ $post['title'] }}
+                </a>
+            </h2>
+            <h5>By: {{ $post['author'] }}</h5>
+            <p>{{ $post['body'] }}/p>
+        </article>
+    @endforeach
+@endsection
+
