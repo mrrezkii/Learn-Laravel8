0816f0566a8446d4b94e632f0c82742cc3305f62
nando <muhammad.rezki.ananda@gmail.com>
2021-12-05 09:55:17 +0700

    Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Muhammad Rezki Ananda",
+        "email" => "muhammad.rezki.ananda@gmail.com",
+        "img" => "motion.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
