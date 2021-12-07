e5d4b784d808bb5276d8b6492dcf8f434700a058
nando <muhammad.rezki.ananda@gmail.com>
2021-12-07 10:46:21 +0700

    Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
+        <p>By. Muhammad Rezki Ananda in <a
+                href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         <h5>{{ $post->author }}</h5>
         <p>{!! $post->body !!}</p>
     </article>
