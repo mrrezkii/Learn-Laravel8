4d305f681fa49c2dbb931ab486f2f79afa1ddd7f
nando <muhammad.rezki.ananda@gmail.com>
2021-12-08 23:47:42 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,64 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Muhammad Rezki Ananda',
+            'email' => 'muhammad.rezki.ananda@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+
+        User::create([
+            'name' => 'Rezki Ananda',
+            'email' => 'muh.rezkiananda@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertana',
+            'category_id' => 1,
+            'user_id' => 1,
+            'slug' => 'judul-pertama',
+            'excerpt' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'category_id' => 1,
+            'user_id' => 1,
+            'slug' => 'judul-kedua',
+            'excerpt' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'category_id' => 2,
+            'user_id' => 1,
+            'slug' => 'judul-ketiga',
+            'excerpt' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'category_id' => 2,
+            'user_id' => 2,
+            'slug' => 'judul-keempat',
+            'excerpt' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt",
+            'body' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,11 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        <p>By. Muhammad Rezki Ananda in <a
+        <p> By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> <a
                 href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         <h5>{{ $post->author }}</h5>
         <p>{!! $post->body !!}</p>
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-5">Back to Posts</a>
 @endsection
