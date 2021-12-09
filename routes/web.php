3b8c212bcdf9f17d7454bdee8bc4e9c2d3759e59
nando <muhammad.rezki.ananda@gmail.com>
2021-12-09 11:39:44 +0700

    Redesign UI

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => "home",
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        "active" => "about",
         "name" => "Muhammad Rezki Ananda",
         "email" => "muhammad.rezki.ananda@gmail.com",
         "img" => "motion.jpg"
@@ -40,6 +42,7 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
+        "active" => "categories",
         'categories' => Category::all()
     ]);
 });
@@ -47,7 +50,8 @@ Route::get('/categories', function () {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post by Category : $category->name",
-        'posts' => $category->posts - load('author', 'category'),
+        "active" => "categories",
+        'posts' => $category->posts->load('author', 'category'),
     ]);
 });
 
